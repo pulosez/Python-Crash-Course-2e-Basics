16cccfcd94032c42822653cbd0cc2e4b5757d5e2
pulosez <pulosez@gmail.com>
2021-07-29 13:18:08 +0300

    Added task 9-11

---
+++ #9: Classes/admin.py
@@ -0,0 +1,7 @@
+from users import Admin
+
+admin_0 = Admin('anna', 'smith', 'asmith', 'annasmith@mail.com', 29)
+privileges_0 = ['can add post', 'can delete post', 'can ban user']
+admin_0.describe_user()
+admin_0.privileges.privileges = privileges_0
+admin_0.privileges.show_privileges()
--- #9: Classes/users.py
+++ #9: Classes/users.py
@@ -1,4 +1,3 @@
-# 9.(3, 5).
 class User:
 
     def __init__(self, first_name, last_name, username, email, age):
@@ -25,7 +24,6 @@ class User:
         self.login_attempts = 0
 
 
-# 9.(7, 8).
 class Privileges:
 
     def __init__(self, privileges=[]):
@@ -42,25 +40,3 @@ class Admin(User):
     def __init__(self, first_name, last_name, username, email, age):
         super().__init__(first_name, last_name, username, email, age)
         self.privileges = Privileges()
-
-
-user_0 = User('john', 'frost', 'jfrost', 'johnfrost@mail.com', 27)
-user_0.describe_user()
-user_0.greet_user()
-
-user_1 = User('may', 'tanaka', 'mtanaka', 'maytanaka@mail.com', 25)
-user_1.describe_user()
-user_1.greet_user()
-
-for value in range(1, 4):
-    user_1.increment_login_attempts()
-print(user_1.login_attempts)
-user_1.reset_login_attempts()
-print(user_1.login_attempts)
-
-
-admin_0 = Admin('anna', 'smith', 'asmith', 'annasmith@mail.com', 29)
-privileges_0 = ['can add post', 'can delete post', 'can ban user']
-admin_0.describe_user()
-admin_0.privileges.privileges = privileges_0
-admin_0.privileges.show_privileges()
