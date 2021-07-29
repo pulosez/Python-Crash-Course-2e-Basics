b90950fec4a15f01b0535b7ec8a8b3b429bbe73a
pulosez <pulosez@gmail.com>
2021-07-30 01:10:21 +0300

    Added theory Chapter 10: Reading from a File

---
+++ #10: Files and Exceptions/file_reader.py
@@ -0,0 +1,14 @@
+file_name = 'text_files/pi_million_digits.txt'
+
+with open(file_name) as file_object:
+    lines = file_object.readlines()
+
+pi_string = ''
+for line in lines:
+    pi_string += line.strip()
+
+birthday = input("Enter your birthday, in the form mmddyy: ")
+if birthday in pi_string:
+    print("Your birthday appears in the first million digits of pi!")
+else:
+    print("Your birthday does not appear in the first million digits of pi.")
