9a9c9615ef3ff428967f9a4627ae56c5806a685f
pulosez <pulosez@gmail.com>
2021-07-29 13:08:05 +0300

    Added theory Chapter 9: Importing Classes

--- #9: Classes/car.py
+++ #9: Classes/car.py
@@ -1,3 +1,6 @@
+"""Клас для моделювання автівки."""
+
+
 class Car:
     """Проста спроба змоделювати машину."""
 
@@ -30,16 +33,3 @@ class Car:
     def increment_odometer(self, miles):
         """Додати задане значення до показника одометра."""
         self.odometer_reading += miles
-
-
-my_new_car = Car('audi', 'a4', 2019)
-print(my_new_car.get_descriptive_name())
-my_new_car.update_odometer(23)
-my_new_car.read_odometer()
-
-my_used_car = Car('subaru', 'outback', 2015)
-print(my_used_car.get_descriptive_name())
-my_used_car.update_odometer(23_500)
-my_used_car.read_odometer()
-my_used_car.increment_odometer(100)
-my_used_car.read_odometer()
--- #9: Classes/electric_car.py
+++ #9: Classes/electric_car.py
@@ -1,27 +1,5 @@
-class Car:
-    """Проста спроба змоделювати машину."""
-
-    def __init__(self, make, model, year):
-        self.make = make
-        self.model = model
-        self.year = year
-        self.odometer_reading = 0
-
-    def get_descriptive_name(self):
-        long_name = f"{self.year} {self.make} {self.model}"
-        return long_name.title()
-
-    def read_odometer(self):
-        print(f"This car has {self.odometer_reading} miles on it.")
-
-    def update_odometer(self, mileage):
-        if mileage >= self.odometer_reading:
-            self.odometer_reading = mileage
-        else:
-            print("You can't roll back an odometer!")
-
-    def increment_odometer(self, miles):
-        self.odometer_reading += miles
+"""Набір класів, що можуть змоделювати електрокар."""
+from car import Car
 
 
 class Battery:
@@ -46,7 +24,6 @@ class Battery:
 
         print(f"This car can go {range} miles on a full charge.")
 
-    # 9.9.
     def upgrade_battery(self):
         if self.battery_size < 100:
             self.battery_size = 100
@@ -64,12 +41,3 @@ class ElectricCar(Car):
         """
         super().__init__(make, model, year)
         self.battery = Battery()
-
-
-my_tesla = ElectricCar('tesla', 'model s', 2019)
-print(my_tesla.get_descriptive_name())
-my_tesla.battery.describe_battery()
-my_tesla.battery.get_range()
-
-my_tesla.battery.upgrade_battery()
-my_tesla.battery.get_range()
