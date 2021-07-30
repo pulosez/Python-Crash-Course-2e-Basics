ec49fe3b75a2763b152bcc19d985677bb5750698
pulosez <pulosez@gmail.com>
2021-07-30 17:53:13 +0300

    Added theory Chapter 10: Writing to a File

---
+++ #10: Files and Exceptions/write_message.py
@@ -0,0 +1,9 @@
+file_name = 'text_files/programming.txt'
+
+with open(file_name, 'w') as file_object:
+    file_object.write("I love programming.\n")
+    file_object.write("I love creating new games.\n")
+
+with open(file_name, 'a') as file_object:
+    file_object.write("I also love finding meaning in large datasets.\n")
+    file_object.write("I love creating apps that can run in browser.\n")
