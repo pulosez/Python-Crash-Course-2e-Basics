8cbde24a1771f01633b0f2b93343e52fa2a04ab7
pulosez <pulosez@gmail.com>
2021-07-31 12:51:35 +0300

    Added task 10-8

---
+++ #10: Files and Exceptions/cats_and_dogs.py
@@ -0,0 +1,11 @@
+# 10.8.
+file_names = ['cats.txt', 'dogs.txt']
+
+for file_name in file_names:
+    try:
+        print(f"Try to open file {file_name} ...")
+        with open(file_name) as f:
+            contents = f.read()
+            print(contents)
+    except FileNotFoundError:
+        print(f"Sorry, it looks file {file_name} is not found.")
